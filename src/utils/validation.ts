be96ee1852d92e601187c41d3905aed23ad1920e
jeylanicodes <83839350+jeylanicodes@users.noreply.github.com>
2022-08-12 20:40:56 +0100

    fix: removed specific validation

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,22 +1,11 @@
 import * as yup from 'yup';
 
-export const quotationSchema = yup
+export const genericSchema = yup
   .object({
     customerEmail: yup.string().required().email(),
     customerName: yup.string().required(),
-    registrationNumber: yup.string().required(),
-    mileage: yup.number().required().moreThan(0),
-    noOfKeepers: yup.string().required(),
-    condition: yup.string().required(),
-    hasMostRecentLogBook: yup.string().required(),
-    requiresRedKey: yup.string().required(),
-    hasOutstandingFinance: yup.string().required(),
-    outstandingFinanceAmount: yup.number().when('hasOutstandingFinance', {
-      is: 'yes',
-      then: yup.number().required().moreThan(0)
-    }),
-    hasIssues: yup.string().required(),
-    issues: yup.string().when('hasIssues', {
+    yesOrNoOption: yup.string().required(),
+    yesExtraOption: yup.string().when('yesOrNoOption', {
       is: 'yes',
       then: yup.string().required()
     })
